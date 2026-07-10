c848015a2abefed6a2a42dae5a6adb02dc1cb788
victor <topgspineivictor@gmail.com>
2026-07-10 16:21:26 +0300

    first frequency

--- src/autocorrelation.cc
+++ src/autocorrelation.cc
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <vector>
+
+int findPeriod(float arr[], int n){
+    float threshold = 0.3 * arr[0];
+
+    for (int k = 100; k < 600; k++) {
+        if (arr[k] > arr[k-1] and
+            arr[k] > arr[k+1] and
+            arr[k] > threshold) {
+            return k;
+        }
+    }
+
+    return -1;
+}
 
 int main() {
     std::freopen("test", "r", stdin);
@@ -15,15 +30,19 @@ int main() {
         std::cin >> inp;
         samples[i] = std::stof(inp);
     }
-    std::cout << "k, R(k)\n";
+    //std::cout << "k, R(k)\n";
     for (int k = 0; k < size; k++) {
         float sum = 0;
         for (int n = 0; n < size - k - 1; n++) {
             sum += samples[n] * samples[n + k];
         }
         autocorrelation[k] = sum;
-        std::cout << k << ", " << sum << '\n';
+        //std::cout << k << ", " << sum << '\n';
     }
     
-
+    //std::cout << findPeriod(autocorrelation, size) << '\n';
+    auto period = findPeriod(autocorrelation, size);
+    int rate = 44100;
+    auto frequency = (float)rate / period;
+    std::cout << "Frequency: " << frequency << " Hz\n";
 }
